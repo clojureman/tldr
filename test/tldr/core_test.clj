2b0535c2093c78c0f5f045e9002ee2e00d77e3a1
Mads Olsen <olsenmads@gmail.com>
2019-09-13 02:06:08 +0200

    more tests

--- test/tldr/core_test.clj
+++ test/tldr/core_test.clj
@@ -11,3 +11,32 @@
             x 800
             (function f [x]
                       (vector x x)))))))
+
+(deftest no-where-block
+  (testing "No where block"
+    (is (= 23 (compute 23)))))
+
+(deftest top-students
+  (testing "Top students example"
+    (is (= 
+         '{:top-students [{:name Zeus, :grade 100} 
+                          {:name Zach, :grade 20}
+                          {:name Peterson, :grade 8}],
+           :avg-grade 27}
+         
+         (compute
+              {:top-students (take 3 (reverse students-by-grade))
+               :avg-grade    (/ (sum student-grades)
+                                (count student-grades))}
+          where
+              sum                (partial apply +)
+              students-by-grade  (sort-by :grade students)
+              student-grades     (map :grade students)
+
+          where
+              students '[{:name Peterson  :grade 8}
+                         {:name Zeus      :grade 100}
+                         {:name Zach      :grade 20}
+                         {:name SleepyJoe :grade 0}
+                         {:name Droopy    :grade 7}])))))
+
